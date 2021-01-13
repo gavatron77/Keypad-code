e6fa012428807bd96fe23dfe30e580d9030500ad
OroArmor <eliorona@live.com>
2021-01-12 17:06:03 -0800

    Clean up code

--- Keypad_code.ino
+++ Keypad_code.ino
@@ -1,150 +1,53 @@
 #include <Adafruit_NeoPixel.h>
 #include <Keyboard.h>
 
-#define ROWS 3
-#define COLUMNS 4
+#include "Keypad_constants.h"
+#include "Keypad_functions.h"
 
-char keyAssign[ROWS][COLUMNS] {
-  {KEY_F13, KEY_F14, KEY_F15, KEY_F16},
-  {KEY_F17, KEY_F18, KEY_F19, KEY_F20},
-  {KEY_F21, KEY_F22, KEY_F23, KEY_F24}
-};
+boolean keys[ROWS * COLUMNS];
+int ledloc[ROWS * COLUMNS];
+boolean written = false;
 
-double dim = 0.1; //Strip brightness
-boolean keys[ROWS][COLUMNS];
-int ledloc[ROWS][COLUMNS];
-int ledCount = sizeof(keys);
-int counter = 0;
+uint32_t colors[ROWS * COLUMNS]; //LED colors are GRBW, for RGB set the third dimension to 3
 
-int rowPins[ROWS] = {
-  0, 1, 2
-};
-
-int colPins[COLUMNS] = {
-  3, 4, 5, 6
-};
-
-int colors[ROWS][COLUMNS][4]; //LED colors are GRBW, for RGB set the third dimension to 3
-
-Adafruit_NeoPixel backlight(ledCount, 7, NEO_GRBW + NEO_KHZ800);
+Adafruit_NeoPixel backlight(ROWS * COLUMNS, 7, NEO_GRBW + NEO_KHZ800);
 
 void setup() {
   Serial.begin(9600);
   backlight.begin();
   Keyboard.begin();
+  
   backlight.setBrightness(255);
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
+  
+  pinMode(H1, INPUT_PULLUP);
+  pinMode(H2, INPUT_PULLUP);
+  pinMode(H3, INPUT_PULLUP);
 
-  writeAll(0, 0, 0, 255);
-  writeColors();
+  pinMode(V1, OUTPUT);
+  pinMode(V2, OUTPUT);
+  pinMode(V3, OUTPUT);
+  pinMode(V4, OUTPUT);
+
+  digitalWrite(V1, HIGH);
+  digitalWrite(V2, HIGH);
+  digitalWrite(V3, HIGH);
+  digitalWrite(V4, HIGH);
+
+  writeAll(0, 0, 0, 255, colors);
+  writeColors(backlight, colors);
 }
-
 
 void loop() {
-  readKeys();
-  updateKeys();
+  readKeys(keys);
+  updateKeys(keys);
 
-  counter++;
-  if (counter == 10) {
-    pressedColors();
-    writeColors();
-    counter = 0;
-  }
-}
-
-
-
-
-
-void printKeys() { //For debugging, prints key status (pushed or not) to Serial console. Make sure to uncomment the Serial.begin line at the top of setup if you use this
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLUMNS; j++) {
-      Serial.print(keys[i][j]);
-      if (j == COLUMNS - 1) {
-        Serial.println();
-      } else {
-        Serial.print(" | ");
-      }
+  if (micros()  % 1000000 > 10000) {
+    if (!written) {
+      pressedColors(colors, keys);
+      writeColors(backlight, colors);
+      written = true;
     }
-  }
-}
-
-void writeColors() { //Writes colors to pixels from the colors array
-  int x = 0;
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLUMNS; j++) {
-      backlight.setPixelColor(x, pixelColor(i, j));
-      x++;
-    }
-  }
-  backlight.show();
-}
-
-void writeAll(int green, int red, int blue, int white) { //Writes an RGBW value to every slot in the light array
-  for (int i = 0;  i < ROWS; i++) {
-    for (int j = 0; j < COLUMNS; j++) {
-      colors[i][j][0] = green;
-      colors[i][j][1] = red;
-      colors[i][j][2] = blue;
-      colors[i][j][3] = white;
-    }
-  }
-}
-
-void pressedColors() { //Demo lighting function that sets all pressed keys to rainbow
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLUMNS; j++) {
-      if (keys[i][j]) {
-        colors[i][j][0] = 255;
-        colors[i][j][1] = 0;
-        colors[i][j][2] = 255;
-        colors[i][j][3] = 0;
-      } else {
-        colors[i][j][0] = 0;
-        colors[i][j][1] = 255;
-        colors[i][j][2] = 255;
-        colors[i][j][3] = 0;
-      }
-    }
-  }
-}
-
-void readKeys() { //Updates key states in the keys array
-  for (int i = 0; i < COLUMNS; i++) {
-    digitalWrite(colPins[i], LOW);
-    for (int j = 0; j < ROWS; j++) {
-      keys[j][i] = !digitalRead(rowPins[j]);
-    }
-    digitalWrite(colPins[i], HIGH);
-  }
-}
-
-uint32_t pixelColor(int i, int j) { //Basically just the strip.Color function from the Adafruit Neopixel library, just for the colors array
-  return ((uint32_t)colors[i][j][3] << 24) | ((uint32_t)colors[i][j][0] << 16) | ((uint32_t)colors[i][j][1] <<  8) | colors[i][j][2];
-}
-
-void updateKeys() {
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLUMNS; j++) {
-      booleanKeyboard(keys[i][j], keyAssign[i][j]);
-    }
-  }
-}
-
-void booleanKeyboard(boolean logic, char key) {
-  if (logic) {
-    Keyboard.press(key);
   } else {
-    Keyboard.release(key);
+    written = false;
   }
 }
--- Keypad_functions.h
+++ Keypad_functions.h
@@ -1,1 +1,72 @@
+#pragma once
+#include "Keypad_constants.h"
 
+void printKeys(boolean* keys) { //For debugging, prints key status (pushed or not) to Serial console. Make sure to uncomment the Serial.begin line at the top of setup if you use this
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      Serial.print(keys[i * ROWS + j]);
+      if (j == COLUMNS - 1) {
+        Serial.println();
+      } else {
+        Serial.print(" | ");
+      }
+    }
+  }
+}
+
+void writeColors(Adafruit_NeoPixel& backlight, uint32_t* colors) { //Writes colors to pixels from the colors array
+  int x = 0;
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      backlight.setPixelColor(x, colors[i * ROWS + j]);
+      x++;
+    }
+  }
+  backlight.show();
+}
+
+void writeAll(int green, int red, int blue, int white, uint32_t* colors) { //Writes an RGBW value to every slot in the light array
+  for (int i = 0;  i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      colors[i * ROWS + j] = Adafruit_NeoPixel::Color(green, red, blue, white);
+    }
+  }
+}
+
+void pressedColors(uint32_t* colors, boolean* keys) { //Demo lighting function that sets all pressed keys to rainbow
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      if (keys[i * ROWS + j]) {
+        colors[i * ROWS + j] = Adafruit_NeoPixel::Color(255, 0, 255, 0);
+      } else {
+        colors[i * ROWS + j] = Adafruit_NeoPixel::Color(0, 255, 255, 0);
+      }
+    }
+  }
+}
+
+void readKeys(boolean* keys) { //Updates key states in the keys array
+  for (int i = 0; i < COLUMNS; i++) {
+    digitalWrite(colPins[i], LOW);
+    for (int j = 0; j < ROWS; j++) {
+      keys[j * ROWS + i] = !digitalRead(rowPins[j]);
+    }
+    digitalWrite(colPins[i], HIGH);
+  }
+}
+
+void booleanKeyboard(boolean logic, char key) {
+  if (logic) {
+    Keyboard.press(key);
+  } else {
+    Keyboard.release(key);
+  }
+}
+
+void updateKeys(boolean* keys) {
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      booleanKeyboard(keys[i * ROWS + j], keyAssign[i * ROWS + j]);
+    }
+  }
+}
